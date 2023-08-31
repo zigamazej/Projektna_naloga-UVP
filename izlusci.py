6f75bbdee33a8d8f7e171e669c90bed8bac36e62
zigamazej <zigamazej8@gmail.com>
2023-08-31 11:01:11 +0200

    popravil pozicije, dodal opise

--- izlusci.py
+++ izlusci.py
@@ -15,10 +15,12 @@ def poisci_bloke(besedilo):
 def izlusci_podatke(blok):
     igralec = {}
     
+    #izluscimo ime in oznako
     vzorec_ime_in_oznaka = re.compile(r'<a href="/players/(?P<oznaka>.+?).html">(?P<ime>.+?)</a>', flags=re.DOTALL,)
     najdba = vzorec_ime_in_oznaka.search(blok)
     igralec["ime"] = najdba["ime"]
 
+    #izluscimo podatke o zacetku in koncu kariere
     vzorec_kariera = re.compile(
         r'<td class="right " data-stat="year_min" >(?P<zacetek_kariere>\d+)</td><td class="right " data-stat="year_max" >(?P<konec_kariere>\d+)</td>'
         )
@@ -26,14 +28,17 @@ def izlusci_podatke(blok):
     igralec["zacetek_kariere"] = int(najdba["zacetek_kariere"])
     igralec["konec_kariere/sedanjost"] = int(najdba["konec_kariere"])
 
+    #izluscimo pozicijo na igriscu, ki je ponavadi igra
     vzorec_pozicija = re.compile(r'<td class="center " data-stat="pos" >(?P<pozicija>.+?)</td>', flags=re.DOTALL,)
     najdba = vzorec_pozicija.search(blok)
-    igralec["pozicija"] = najdba["pozicija"]
+    igralec["pozicija"] = najdba["pozicija"][0]  #nekateri igralci lahko igrajo 2 poziciji, vzamemo le prvo, ki je preferirana
 
+    #izluscimo visino
     vzorec_visina = re.compile(r'<td class="right " data-stat="height" csk="(?P<csk>.+?)" >(?P<feet>\d+)-(?P<inches>\d+)</td>')
     najdba = vzorec_visina.search(blok)
     igralec["visina"] = round(30.48 * int(najdba["feet"]) + 2.54 * int(najdba["inches"]))
 
+    #izluscimo tezo
     vzorec_teza = re.compile(r'<td class="right " data-stat="weight" >(?P<teza>\d+)</td>')
     najdba = vzorec_teza.search(blok)
     if najdba:
@@ -41,6 +46,7 @@ def izlusci_podatke(blok):
     else:
         igralec["teza"] = None
 
+    #izluscimo leto rojstva
     vzorec_datum_rojstva = re.compile(r'<td class="left " data-stat="birth_date" csk="(?P<datum_rojstva>\d+)" >')
     najdba = vzorec_datum_rojstva.search(blok)
     if najdba:
@@ -48,26 +54,18 @@ def izlusci_podatke(blok):
     else:    
         igralec["leto_rojstva"] = None
 
+    #izluscimo ali je igralec v Hall of fame
     if "*" in blok:
         igralec["v_hall_of_fame"] = "da"
     else:
         igralec["v_hall_of_fame"] = "ne"
 
+    #izluscimo ali je igralec se aktiven
     if "<strong>" in blok:
         igralec["trenutno_v_ligi"] = "da"
     else:
         igralec["trenutno_v_ligi"] = "ne"
 
-#    vzorec_college = re.compile(
-#        #r'<td class="left " data-stat="colleges" >'
-#        r"<a href='/friv/colleges.fcgi?college=(?P<col>.+?)'>",
-#        flags=re.DOTALL,
-#        )
-#    najdba = vzorec_college.search(blok)
-#    if najdba:
-#        igralec["college"] = najdba["college"]
-#    else:
-#        igralec["college"] = None
 
 
     return igralec
